5b20a066290f6b9c816980950dcab31d44664efc
huangxuan <huangxuan@didichuxing.com>
2019-10-10 16:58:05 +0800

    1.0.4

---
+++ src/commands/dev/dev.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+
+import pkg from '../../../package.json';
+import DevServer from '../../util/dev/server';
+import parseListen from '../../util/dev/parse-listen';
+import { Output } from '../../util/output';
+import { NowContext } from '../../types';
+
+type Options = {
+  '--debug'?: boolean;
+  '--listen'?: string;
+};
+
+export default async function dev(
+  ctx: NowContext,
+  opts: Options,
+  args: string[],
+  output: Output
+) {
+  output.dim(`FDS CLI ${pkg.version} Started.`);
+
+  const [dir = '.'] = args;
+  const cwd = path.resolve(dir);
+  const listen = parseListen(opts['--listen'] || '3000');
+  const debug = opts['--debug'] || false;
+  const devServer = new DevServer(cwd, { output, debug });
+
+  process.once('SIGINT', () => devServer.stop());
+
+  await devServer.start(...listen);
+}
---
+++ src/util/ua.ts
@@ -0,0 +1,6 @@
+import os from 'os';
+import pkg from './pkg';
+
+export default `now ${pkg.version} node-${
+  process.version
+} ${os.platform()} (${os.arch()})`;
